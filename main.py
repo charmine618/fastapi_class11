523216c8224b236ef752c7d90b01fb31f05405ac
charmine618 <charmine.leu@stonybrook.edu>
2023-05-12 23:30:56 +0000

    updated main.py file

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 # main.py
 from fastapi import FastAPI
+import pandas as pd
+
+df = pd.read_csv('./data/utilization2019.csv')
 
 app = FastAPI()
 
-@app.get("/")
+@app.get('/')
 async def root():
-    return {"message": "Hello World"}
+    return {'this is a API service for MN healthcare utilization code details'}
+
+@app.get('/preview')
+async def preview():
+    top10rows = df.head(1)
+    result = top10rows.to_json(orient="records")
+    return {result}
+
+@app.get("/ccd/{value}")
+async def ccode(value):
+    print('value: ', value)
+    filtered = df[df['county_code'] == value]
+    if len(filtered) <= 0:
+        return {'There is nothing here'}
+    else: 
+        return {filtered.to_json(orient="records")}
+
+@app.get('/ccd/{value}/sex/{value2}')
+async def ccode2(value, value2):
+    filtered = df[df['county_code'] == value]
+    filtered2 = filtered[filtered['sex'] == value2]
+    if len(filtered2) <= 0:
+        return {'There is nothing here'}
+    else: 
+        return {filtered2.to_json(orient="records")}    
